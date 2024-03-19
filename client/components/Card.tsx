d6f29899f125b2ec1266e144a4a4cf9fe028c171
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-03-19 15:39:38 +1300

    Add some fake card images

--- client/components/Card.tsx
+++ client/components/Card.tsx
@@ -1,19 +1,21 @@
 interface Card {
   id: number
   imgUrl: string
-  tags: string[]
 }
 
 interface Props {
   card: Card
 }
 
+// 420 x 525 = 0.8
+const maxh = 100
+const maxw = maxh*0.8
+
 function Card({ card }: Props) {
   return (
     <>
       <div className=" border-2 rounded-md p-5 my-2">
-        <img className="h-72 w-44" src={card.imgUrl} />
-        <div className="text-xl mt-2">{card.tags.join(', ')}</div>
+        <img className=" h-60 aspect-w-8 aspect-h-10" src={card.imgUrl} />
       </div>
     </>
   )
--- client/components/CardGallery.tsx
+++ client/components/CardGallery.tsx
@@ -2,16 +2,16 @@ import Card from './Card'
 
 function CardGallery() {
   const cardData: Card[] = [
-    { id: 1, imgUrl: 'https://example.com', tags: ['tag1', 'tag2'] },
-    { id: 2, imgUrl: 'https://example.com', tags: ['tag3', 'tag2'] },
-    { id: 3, imgUrl: 'https://example.com', tags: ['tag2', 'tag4'] },
+    { id: 1, imgUrl: 'https://threadheads.com/cdn/shop/files/GreetingCards-085.jpg'},
+    { id: 2, imgUrl: 'https://threadheads.com/cdn/shop/files/GreetingCards-089.jpg'},
+    { id: 3, imgUrl: 'https://threadheads.com/cdn/shop/files/GreetingCards-097.jpg'},
   ]
-
+  
   return (
     <>
-      <div className=" text-xl mt-2">CardGallery</div>
+      <div className=" text-xl mt-2">Card Gallery</div>
       <div className="flex flex-wrap gap-2">
-        {cardData.map((card) => {
+        {cardData?.map((card) => {
           return <Card key={card.id} card={card} />
         })}
       </div>
