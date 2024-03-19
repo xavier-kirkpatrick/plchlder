c3ef6c96afbc2a30694af75f0aa2f09e32b9b56a
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-03-19 15:25:59 +1300

    Add fake card data and an interface

--- client/components/Card.tsx
+++ client/components/Card.tsx
@@ -1,7 +1,20 @@
-function Card() {
+interface Card {
+  id: number
+  imgUrl: string
+  tags: string[]
+}
+
+interface Props {
+  card: Card
+}
+
+function Card({ card }: Props) {
   return (
     <>
-      <div className=" border-2 rounded-md p-5 h-72 w-44 my-2">A card</div>
+      <div className=" border-2 rounded-md p-5 my-2">
+        <img className="h-72 w-44" src={card.imgUrl} />
+        <div className="text-xl mt-2">{card.tags.join(', ')}</div>
+      </div>
     </>
   )
 }
--- client/components/CardGallery.tsx
+++ client/components/CardGallery.tsx
@@ -1,14 +1,19 @@
 import Card from './Card'
 
 function CardGallery() {
+  const cardData: Card[] = [
+    { id: 1, imgUrl: 'https://example.com', tags: ['tag1', 'tag2'] },
+    { id: 2, imgUrl: 'https://example.com', tags: ['tag3', 'tag2'] },
+    { id: 3, imgUrl: 'https://example.com', tags: ['tag2', 'tag4'] },
+  ]
+
   return (
     <>
       <div className=" text-xl mt-2">CardGallery</div>
       <div className="flex flex-wrap gap-2">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {cardData.map((card) => {
+          return <Card key={card.id} card={card} />
+        })}
       </div>
     </>
   )
