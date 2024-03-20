b171e62a2b4eefd6ef727a6b665834696dbc4108
Xavier Kirkpatrick <kirkpatrickxavier@gmail.com>
2024-03-20 17:16:42 +1300

    little extra thank you

--- client/components/CardGallery.tsx
+++ client/components/CardGallery.tsx
@@ -8,6 +8,7 @@ function CardGallery() {
   return (
     <>
       <div className=" text-xl mt-2">Card Gallery</div>
+      <div>Hope you enjoy your cards</div>
       <div className="flex flex-wrap gap-2">
         {cards.data?.map((card) => {
           return <Card key={card.id} card={card} />
