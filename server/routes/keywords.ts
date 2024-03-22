6d6311b69b18044edecfece43ae6f23fb85060d5
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-03-23 10:40:41 +1300

    Add a scraper service with mock data

--- server/routes/keywords.ts
+++ server/routes/keywords.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { parseToKeywords, scrapeProfile } from '../services/scraper'
 // import { JwtRequest } from '../auth0.ts'
 
 const router = express.Router()
@@ -8,11 +9,19 @@ const router = express.Router()
 router.get('/', async (req, res) => {
   try {
     const username = req.query.username
-    // TODO: call a function to scrape ig profile
-    // TODO: parse through an LLM to convert to keywords
+
+    // ensure username is just a string
+    if (typeof username !== 'string') {
+      return res.status(400).send('Bad request')
+    }
+
+    // Call function to scrape profile
+    const profileData = await scrapeProfile(username)
+
+    // Parse scraped data through an LLM to convert to keywords
+    const keywords = await parseToKeywords(profileData)
 
     // mock returned keywords
-    const keywords = ['dogs', 'mountains', 'chilli']
     res.json({ keywords })
 
   } catch (error) {
@@ -20,99 +29,5 @@ router.get('/', async (req, res) => {
     res.status(500).send('Something went wrong')
   }
 })
-
-// TODO: use checkJwt as middleware
-// POST /api/v1/fruits
-// router.post('/', async (req: JwtRequest, res) => {
-//   const { fruit } = req.body as { fruit: FruitData }
-//   const auth0Id = req.auth?.sub
-
-//   if (!fruit) {
-//     console.error('No fruit')
-//     return res.status(400).send('Bad request')
-//   }
-
-//   if (!auth0Id) {
-//     console.error('No auth0Id')
-//     return res.status(401).send('Unauthorized')
-//   }
-
-//   try {
-//     const newFruit = await db.addFruit(fruit, auth0Id)
-
-//     res.status(201).json({ fruit: newFruit })
-//   } catch (error) {
-//     console.error(error)
-//     res.status(500).send('Something went wrong')
-//   }
-// })
-
-// // TODO: use checkJwt as middleware
-// // PUT /api/v1/fruits
-// router.put('/:id', async (req: JwtRequest, res) => {
-//   const { fruit } = req.body as { fruit: FruitData }
-//   const auth0Id = req.auth?.sub
-
-//   const id = Number(req.params.id)
-
-//   if (!fruit || !id) {
-//     console.error('Bad Request - no fruit or id')
-//     return res.status(400).send('Bad request')
-//   }
-
-//   if (!auth0Id) {
-//     console.error('No auth0Id')
-//     return res.status(401).send('Unauthorized')
-//   }
-
-//   try {
-//     await db.userCanEdit(id, auth0Id)
-//     const updatedFruit = await db.updateFruit(id, fruit)
-
-//     res.status(200).json({ fruit: updatedFruit })
-//   } catch (error) {
-//     if (error instanceof Error) {
-//       console.error(error)
-//       if (error instanceof Error && error.message === 'Unauthorized') {
-//         return res
-//           .status(403)
-//           .send('Unauthorized: Only the user who added the fruit may update it')
-//       }
-//       res.status(500).send('Something went wrong')
-//     }
-//   }
-// })
-
-// // TODO: use checkJwt as middleware
-// // DELETE /api/v1/fruits
-// router.delete('/:id', async (req: JwtRequest, res) => {
-//   const id = Number(req.params.id)
-//   const auth0Id = req.auth?.sub
-
-//   if (!id) {
-//     console.error('Invalid fruit id')
-//     return res.status(400).send('Bad request')
-//   }
-
-//   if (!auth0Id) {
-//     console.error('No auth0Id')
-//     return res.status(401).send('Unauthorized')
-//   }
-
-//   try {
-//     await db.userCanEdit(id, auth0Id)
-//     await db.deleteFruit(id)
-
-//     res.sendStatus(200)
-//   } catch (error) {
-//     console.error(error)
-//     if (error instanceof Error && error.message === 'Unauthorized') {
-//       return res
-//         .status(403)
-//         .send('Unauthorized: Only the user who added the fruit may update it')
-//     }
-//     res.status(500).send('Something went wrong')
-//   }
-// })
 
 export default router
---
+++ server/services/scraper.ts
@@ -0,0 +1,84 @@
+// server/services/instagramService.ts
+
+/**
+ * Scrapes the profile information for a user.
+ * @param username - The username of the profile to scrape.
+ * @returns An array of strings representing the scraped profile data.
+ */
+export function scrapeProfile(username: string): string[] {
+    console.log("Scraping: ", username);
+
+    // Return the scraped data
+    // TODO: replace mock data
+    return [
+        "some #streetart spotted around Flinders St",
+        "Final few days in Thailand",
+        "Wat Phra That Doi Suthep",
+        "Big Buddha (66 floors to the bindi spot/lookout) and Tham Luang Cave where the thai kids got rescued",
+        "the white temple (some guy's art project) in Chiang Rai",
+        "A red panda, a monkey and a bunch of rust bucket utes, not that you care, chur",
+        "Sorry not sorry, more #streetart discoveries in #chiangmai"
+    ];
+};
+
+
+/**
+ * Parses the given profile data and returns an array of keywords.
+ *
+ * @param {string[]} profileData - The profile data to be parsed.
+ * @return {string[]} An array of keywords extracted from the profile data.
+ */
+export function parseToKeywords(profileData: string[]) {
+    console.log("Parsing: ", profileData)
+
+    // Return the keywords array
+    // TODO: replace mock data
+    return [
+        'streetart',
+        'Flinders Street',
+        'Thailand',
+        'final days',
+        'Wat Phra That Doi Suthep',
+        'temple',
+        'Big Buddha',
+        'lookout',
+        'Tham Luang Cave',
+        'rescue',
+        'white temple',
+        'art project',
+        'Chiang Rai',
+        'red panda',
+        'monkey',
+        'utes',
+        'Chiang Mai'
+    ]
+
+};
+
+
+
+/**
+ * Retrieves person description data based on the provided keywords.
+ *
+ * @param {string[]} keywords - An array of keywords to search for.
+ * @return {Object} - An object containing ??
+ */
+export function getPersonDescriptionData(keywords: string[]) {
+
+    // TODO: replace mock data
+    // Not sure what format we need it in to pass to the image generation API
+    // TODO: Define an interface/type for the return data
+    return {
+        interests: ['Travel', 'Art', 'Culture', 'Adventure', 'Wildlife'],
+        traits: ['Art Enthusiast', 'Explorer', 'Culturally Curious', 'Adventurous'],
+        focusAreas: ['Street Art', 'Historical Landmarks', 'Wildlife Observation'],
+        regionsVisited: ['Southeast Asia', 'Thailand'],
+        noteworthyExperiences: [
+            'Exploring urban street art',
+            'Visiting significant temples and cultural sites',
+            'Adventurous excursions to caves and lookout points',
+            'Observing exotic wildlife'
+        ]
+    };
+
+}
